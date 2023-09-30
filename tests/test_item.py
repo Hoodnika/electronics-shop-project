402a95749efd2301654f95a30596221faa3c6d4b
Hoodnika <Hoodnika213@gmail.com>
2023-09-30 20:51:21 +0500

    add repr and str methods for Item, and test for methods

--- tests/test_item.py
+++ tests/test_item.py
@@ -13,10 +13,24 @@ def test_instantiate_from_csv():
     assert Item.instantiate_from_csv("src/items.csv")[0].name == "Смартфон"
 
 def test_calculate_total_price():
-    dekstop = Item("Ноутбук", 50000, 3)
-    assert dekstop.calculate_total_price() == 150000
+    laptop = Item("Ноутбук", 50000, 3)
+    assert laptop.calculate_total_price() == 150000
 
 def test_apply_discount():
-    dekstop2 = Item("Ноутбук", 50000, 3)
+    laptop2 = Item("Ноутбук", 50000, 3)
     Item.pay_rate = 0.9
-    assert dekstop2.apply_discount() == 45000
+    assert laptop2.apply_discount() == 45000
+
+def test_name_getter():
+    laptop3 = Item("Ноутбук", 80000, 1)
+    assert laptop3.name == "Ноутбук"
+
+def test_name_setter():
+    laptop4 = Item("Ноутбук", 200000, 1)
+    laptop4.name = "МегаМощныйНоутбук"
+    assert laptop4.name == "МегаМощный"
+
+def test_perp_str():
+    device = Item("Iphone", 100000, 5)
+    assert str(device) == "Iphone"
+    assert repr(device) == "Item('Iphone', 100000, 5)"
