17a5ab30d51cf76aa8ed22ae8938488f03c012a3
Hoodnika <Hoodnika213@gmail.com>
2023-10-19 17:15:10 +0500

    add super().__init__

--- src/item.py
+++ src/item.py
@@ -43,7 +43,7 @@ class Item:
         self.__name = name
         self.price = price
         self.quantity = quantity
-        # self.all.append(self)
+        super().__init__()
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
